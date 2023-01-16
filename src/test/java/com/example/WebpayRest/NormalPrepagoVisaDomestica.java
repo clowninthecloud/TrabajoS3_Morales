086f9329d0170fc8a7e73a5bb45cf6f2e9213460
Claudio Morales <contacto@claudiomorales.in>
2023-01-15 22:01:11 -0300

    Casi listo!

--- src/test/java/com/example/WebpayRest/NormalPrepagoVisaDomestica.java
+++ src/test/java/com/example/WebpayRest/NormalPrepagoVisaDomestica.java
@@ -35,18 +35,21 @@ public class NormalPrepagoVisaDomestica {
     driver.findElement(By.id("ewebpayTestForm:ewptdAmount")).clear();
     driver.findElement(By.id("ewebpayTestForm:ewptdAmount")).sendKeys("8888");
     driver.findElement(By.xpath("//button[@id='ewebpayTestForm:j_idt32']/span")).click();
+    Thread.sleep(3000);//para que no vaya tan rapido
     driver.findElement(By.xpath("//button[@id='tarjetas']/div[2]/div")).click();
     driver.findElement(By.id("card-number")).click();
     driver.findElement(By.id("card-number")).clear();
     driver.findElement(By.id("card-number")).sendKeys("4670 1579 7056 7383");
     driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Número de tarjeta'])[1]/following::button[1]")).click();
+    Thread.sleep(3000);//para que no vaya tan rapido
     driver.findElement(By.id("card-exp")).click();
     driver.findElement(By.id("card-exp")).clear();
-    driver.findElement(By.id("card-exp")).sendKeys("12/23");
+    driver.findElement(By.id("card-exp")).sendKeys("1223");
     driver.findElement(By.id("card-cvv")).click();
     driver.findElement(By.id("card-cvv")).clear();
     driver.findElement(By.id("card-cvv")).sendKeys("123");
     driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='CVV'])[1]/following::button[3]")).click();
+    Thread.sleep(4000);//para que no vaya tan rapido
     driver.findElement(By.name("rut")).clear();
     driver.findElement(By.name("rut")).sendKeys("10726100-1");
     driver.findElement(By.name("clave")).clear();
