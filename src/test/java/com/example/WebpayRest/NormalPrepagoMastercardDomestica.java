41ec2f97de59b382c739353f7de397b212aaec66
Claudio Morales <contacto@claudiomorales.in>
2023-01-15 21:55:12 -0300

    driver.manage().window().maximize();

--- src/test/java/com/example/WebpayRest/NormalPrepagoMastercardDomestica.java
+++ src/test/java/com/example/WebpayRest/NormalPrepagoMastercardDomestica.java
@@ -38,17 +38,21 @@ public class NormalPrepagoMastercardDomestica {
     driver.findElement(By.id("amount")).sendKeys("5555");
     driver.findElement(By.xpath("//input[@value='Continuar']")).click();
     driver.findElement(By.xpath("//input[@value='Continuar »']")).click();
+    Thread.sleep(3000);//para que no vaya tan rapido
     driver.findElement(By.xpath("//button[@id='tarjetas']/div[2]/span")).click();
     driver.findElement(By.id("card-number")).click();
     driver.findElement(By.id("card-number")).clear();
     driver.findElement(By.id("card-number")).sendKeys("5319 2600 2177 6070");
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Número de tarjeta'])[1]/following::button[1]")).click();
+    Thread.sleep(2000);//para que no vaya tan rapido
     driver.findElement(By.id("card-exp")).click();
     driver.findElement(By.id("card-exp")).clear();
-    driver.findElement(By.id("card-exp")).sendKeys("12/22");
+    driver.findElement(By.id("card-exp")).sendKeys("1223");
     driver.findElement(By.id("card-cvv")).click();
     driver.findElement(By.id("card-cvv")).clear();
     driver.findElement(By.id("card-cvv")).sendKeys("123");
     driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='CVV'])[1]/following::button[3]")).click();
+    Thread.sleep(4000);//para que no vaya tan rapido
     driver.findElement(By.name("rut")).clear();
     driver.findElement(By.name("rut")).sendKeys("10726100-1");
     driver.findElement(By.name("clave")).clear();
@@ -56,60 +60,12 @@ public class NormalPrepagoMastercardDomestica {
     driver.findElement(By.xpath("//input[@value='Aceptar']")).click();
     driver.findElement(By.xpath("//input[@value='Seguir (setResultAut)']")).click();
     driver.findElement(By.xpath("//input[@value='Seguir (retorno a Transbank)']")).click();
+    Thread.sleep(4000);//para que no vaya tan rapido
     driver.findElement(By.name("apiversion")).click();
     new Select(driver.findElement(By.name("apiversion"))).selectByVisibleText("v1.2/");
     driver.findElement(By.xpath("//input[@value='Continuar »']")).click();
-    driver.findElement(By.xpath("//a[contains(text(),'Tienda\n                    normal')]")).click();
-    driver.findElement(By.xpath("//input[@value='Continuar']")).click();
-    driver.findElement(By.xpath("//input[@value='Continuar »']")).click();
-    driver.findElement(By.xpath("//button[@id='tarjetas']/div[2]/span")).click();
-    driver.findElement(By.id("card-number")).click();
-    driver.findElement(By.id("card-number")).clear();
-    driver.findElement(By.id("card-number")).sendKeys("5319 2600 2177 6070");
-    driver.findElement(By.id("card-exp")).click();
-    driver.findElement(By.id("card-exp")).clear();
-    driver.findElement(By.id("card-exp")).sendKeys("12/22");
-    driver.findElement(By.id("card-cvv")).click();
-    driver.findElement(By.id("card-cvv")).clear();
-    driver.findElement(By.id("card-cvv")).sendKeys("123");
-    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='CVV'])[1]/following::button[3]")).click();
-    driver.findElement(By.name("rut")).clear();
-    driver.findElement(By.name("rut")).sendKeys("10726100-1");
-    driver.findElement(By.name("clave")).clear();
-    driver.findElement(By.name("clave")).sendKeys("tbk.2020");
-    driver.findElement(By.xpath("//input[@value='Aceptar']")).click();
-    driver.findElement(By.xpath("//body")).click();
-    driver.findElement(By.xpath("//input[@value='Seguir (setResultAut)']")).click();
-    driver.findElement(By.xpath("//input[@value='Seguir (retorno a Transbank)']")).click();
-    driver.findElement(By.name("apiversion")).click();
-    new Select(driver.findElement(By.name("apiversion"))).selectByVisibleText("v1.2/");
-    driver.findElement(By.xpath("//input[@value='Continuar »']")).click();
-    driver.findElement(By.xpath("//a[contains(text(),'Tienda\n                    normal')]")).click();
-    driver.findElement(By.name("apiversion")).click();
-    new Select(driver.findElement(By.name("apiversion"))).selectByVisibleText("v1.3/");
-    driver.findElement(By.xpath("//*/text()[normalize-space(.)='']/parent::*")).click();
-    driver.findElement(By.id("amount")).clear();
-    driver.findElement(By.id("amount")).sendKeys("5001");
-    driver.findElement(By.xpath("//input[@value='Continuar']")).click();
-    driver.findElement(By.xpath("//input[@value='Continuar »']")).click();
-    driver.findElement(By.xpath("//button[@id='tarjetas']/div[2]")).click();
-    driver.findElement(By.id("card-number")).click();
-    driver.findElement(By.id("card-number")).clear();
-    driver.findElement(By.id("card-number")).sendKeys("5186 1740 0591 2785");
-    driver.findElement(By.id("card-exp")).click();
-    driver.findElement(By.id("card-exp")).clear();
-    driver.findElement(By.id("card-exp")).sendKeys("12/22");
-    driver.findElement(By.id("card-cvv")).click();
-    driver.findElement(By.id("card-cvv")).clear();
-    driver.findElement(By.id("card-cvv")).sendKeys("123");
-    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Sin cuotas y un mes de gracia'])[1]/following::button[1]")).click();
-    driver.findElement(By.xpath("//input[@value='Aceptar']")).click();
-    driver.findElement(By.xpath("//input[@value='Seguir (setResultAut)']")).click();
-    driver.findElement(By.xpath("//input[@value='Seguir (retorno a Transbank)']")).click();
-    driver.findElement(By.name("apiversion")).click();
-    new Select(driver.findElement(By.name("apiversion"))).selectByVisibleText("v1.3/");
-    driver.findElement(By.xpath("//input[@value='Continuar »']")).click();
-    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='RESPONSE STATUS'])[1]/following::div[2]")).click();
+    Thread.sleep(4000);//para que no vaya tan rapido
+
   }
 
   @AfterClass(alwaysRun = true)
