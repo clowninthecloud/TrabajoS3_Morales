6c0714790a73bcd01ac3091f2a8576acecd58f5b
Claudio Morales <contacto@claudiomorales.in>
2023-01-15 21:36:04 -0300

    webdriver.chrome.driver

--- src/test/java/com/example/WebpayRest/NormalDebitVisaDomestica.java
+++ src/test/java/com/example/WebpayRest/NormalDebitVisaDomestica.java
@@ -19,7 +19,7 @@ public class NormalDebitVisaDomestica {
 
   @BeforeClass(alwaysRun = true)
   public void setUp() throws Exception {
-    System.setProperty("webdriver.chrome.driver", "");
+    System.setProperty("webdriver.chrome.driver", "D:\\TBK\\Automatizacion\\src\\test\\resources\\windows\\chromedriver.exe");
     driver = new ChromeDriver();
     baseUrl = "https://www.google.com/";
    // driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
